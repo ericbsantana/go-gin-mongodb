3e6858d7564cbbe18b3c9ae606293a4584700664
Eric Santana <bitencourteric@hotmail.com>
2024-03-29 00:50:29 -0300

    fix: add json tag to _id in user model struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	ID       primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
 	Username string             `json:"username"`
 	Email    string             `json:"email"`
 }
